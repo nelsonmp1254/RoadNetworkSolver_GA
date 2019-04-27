5781441b1942ea6cbf8c71ba915f4c837521505d
Goeksw <50060366+Goeksw@users.noreply.github.com>
2019-04-27 19:40:00 -0400

    Add files via upload
    Implemented Random Walk / initial population generator

--- FinalAlg.py
+++ FinalAlg.py
@@ -3,12 +3,14 @@ from deap import base
 from deap import creator
 from deap import tools
 from enum import Enum
+import random
 
 grid = None
 
 # register operators & create toolbox
 toolbox = base.Toolbox()
 toolbox.register('crossover', PICK_A_CROSSOVER_FUNCTION)
+# Recommend altering either cxPartialyMatched, cxUniformPartialyMatched, or cxOrdered
 toolbox.register('mutate', PICK_A_MUTATION_FUNCTION)
 toolbox.register('select', SELECT_INDIVIDUALS_TO_BREED)
 
@@ -44,9 +46,6 @@ class Path:
         self.route.append(n)
 
 
-
-
-
 creator.create('Individual', Path)
 
 
@@ -61,6 +60,11 @@ def main():
     crossProb = 0.2
     mutProb = 0.2
     generations = 0
+    pop = {}
+    startPop = 200 # starting population size
+
+    startNode = # fist city here
+    endNode = # second city here
 
     # open & read file data
     filename = input("Enter the name of your data file")
@@ -73,8 +77,54 @@ def main():
     height = None
     blank = Node({})
     grid = [[blank for x in range(width)] for y in range(height)]
-
+    nodeToAdd = None
     # now for the randomwalk
+    #
+    #    7     0     1
+    #     \    |    /
+    #    6-   Node  -2
+    #     /    |    \
+    #    5     4     3
+    #
+    #
+
+    for i in range(startPop):
+        lastNode = startNode
+        startingPath = {}
+        while nodeToAdd != endNode:
+            dir = random.randint(0, 7)
+            xoffset = 0
+            yoffset = 0
+            if dir == 0:
+                xoffset = 0
+                yoffset = -1
+            elif dir == 1:
+                xoffset = 1
+                yoffset = -1
+            elif dir == 2:
+                xoffset = 1
+                yoffset = 0
+            elif dir == 3:
+                xoffset = 1
+                yoffset = 1
+            elif dir == 4:
+                xoffset = 0
+                yoffset = 1
+            elif dir == 5:
+                xoffset = -1
+                yoffset = 1
+            elif dir == 6:
+                xoffset = -1
+                yoffset = 0
+            elif dir == 7:
+                xoffset = -1
+                yoffset = -1
+
+            if lastNode.x + xoffset < len(grid) and lastNode.x + xoffset > 0:
+                if lastNode.y + yoffset < len(grid[1]) and lastNode.y + yoffset > 0:
+                    nodeToAdd = grid[lastNode.x + xoffset][lastNode.y + yoffset]
+                    startingPath.append(Node(nodeToAdd))
+        pop.append(Path(startingPath))
 
 
 
